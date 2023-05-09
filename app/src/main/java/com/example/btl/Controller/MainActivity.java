82a80bd4e7ae37c9d94ef2eec48b8c71c4f9c848
PhucNH <PhucNH0303@gmail.com>
2023-05-09 19:45:32 +0700

    add register form

--- app/src/main/java/com/example/btl/Controller/MainActivity.java
+++ app/src/main/java/com/example/btl/Controller/MainActivity.java
@@ -1,14 +1,16 @@
-package com.example.btl;
+package com.example.btl.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.btl.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.register);
     }
 }
