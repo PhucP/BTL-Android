8e5c715f068d089d7b41e26713337128e5fb7bdf
PhucNH <PhucNH0303@gmail.com>
2023-05-14 15:49:30 +0700

    finish update user profile

--- app/src/main/java/com/example/btl_android/Main/Fragment/FragmentUser.java
+++ app/src/main/java/com/example/btl_android/Main/Fragment/FragmentUser.java
@@ -1,5 +1,6 @@
 package com.example.btl_android.Main.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +12,18 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.btl_android.LoginActivity;
+import com.example.btl_android.MainActivity;
 import com.example.btl_android.R;
 import com.example.btl_android.RoomDatabase.Entity.User;
+import com.example.btl_android.UpdateUserActivity;
 import com.google.android.material.tabs.TabLayout;
 
 public class FragmentUser extends Fragment {
+    private static final int REQUEST_CODE_UPDATE_USER = 0303;
+    private static final int RESULT_OK = -1;
     private User currentUser;
-    private TextView name, email, phone, dob, username;
+    private TextView name, email, phone, dob, username, updateUser;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,17 +51,44 @@ public class FragmentUser extends Fragment {
         phone = view.findViewById(R.id.phone);
         dob = view.findViewById(R.id.dob);
         username = view.findViewById(R.id.userName);
+        updateUser = view.findViewById(R.id.updateProfile);
     }
 
     private void activity() {
-        setCurrentUser();
+
+        setCurrentUser(currentUser);
+        updateUserProfile();
     }
 
-    private void setCurrentUser() {
-        name.setText(currentUser.getName());
-        email.setText(currentUser.getEmail());
-        phone.setText(currentUser.getPhoneNumber());
-        dob.setText(currentUser.getBirthday());
-        username.setText(currentUser.getUserName());
+    private void setCurrentUser(User user) {
+        name.setText(user.getName());
+        email.setText(user.getEmail());
+        phone.setText(user.getPhoneNumber());
+        dob.setText(user.getBirthday());
+        username.setText(user.getUserName());
+    }
+
+    private void updateUserProfile() {
+        updateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UpdateUserActivity.class);
+                intent.putExtra("currentUser", currentUser);
+                startActivityForResult(intent, REQUEST_CODE_UPDATE_USER);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE_UPDATE_USER && resultCode == RESULT_OK) {
+            if (data != null && data.hasExtra("updateUser")) {
+                User updatedUser = (User) data.getSerializableExtra("updateUser");
+                currentUser = updatedUser;
+                setCurrentUser(currentUser);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/btl_android/UpdateUserActivity.java
@@ -0,0 +1,96 @@
+package com.example.btl_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.btl_android.RoomDatabase.AppDatabase;
+import com.example.btl_android.RoomDatabase.Entity.User;
+
+public class UpdateUserActivity extends AppCompatActivity {
+    private User currentUser;
+    private EditText name, email, dob, phone;
+    private Button update;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_user);
+
+        init();
+        activity();
+    }
+
+    private void init() {
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+        dob = findViewById(R.id.dob);
+        update = findViewById(R.id.btnUpdate);
+
+        //get user from other form
+        Intent intent = getIntent();
+        currentUser = (User) intent.getSerializableExtra("currentUser");
+    }
+
+    private void activity() {
+        setCurrentUser();
+        updateUserProfile();
+    }
+
+    private void setCurrentUser() {
+        name.setText(currentUser.getName());
+        email.setText(currentUser.getEmail());
+        phone.setText(currentUser.getPhoneNumber());
+        dob.setText(currentUser.getBirthday());
+    }
+
+    private void updateUserProfile() {
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String updateName = name.getText().toString().trim();
+                String updateEmail = email.getText().toString().trim();
+                String updatePhone = phone.getText().toString().trim();
+                String updateDob = dob.getText().toString().trim();
+
+                User updateUser = new User();
+                updateUser.setId(currentUser.getId());
+                updateUser.setUserName(currentUser.getUserName());
+                updateUser.setPassWord(currentUser.getPassWord());
+                updateUser.setName(updateName);
+                updateUser.setEmail(updateEmail);
+                updateUser.setBirthday(updateDob);
+                updateUser.setPhoneNumber(updatePhone);
+
+                Intent intent = getIntent();
+                intent.putExtra("updateUser", updateUser);
+                setResult(RESULT_OK, getIntent());
+
+                UpdateUserTask task = new UpdateUserTask();
+                task.execute(updateUser);
+            }
+        });
+    }
+
+    private class UpdateUserTask extends AsyncTask<User, Void, Void> {
+        @Override
+        protected Void doInBackground(User... users) {
+            AppDatabase db = AppDatabase.getDatabase(getApplicationContext());
+            db.userDao().updateUser(users[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Toast.makeText(UpdateUserActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+}
